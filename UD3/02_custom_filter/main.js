ef43b69ce2cb2308ad37bdeca44ef47ade048929
DESKTOP-S6DAD6J\A8-PC100 <asd@svalero.com>
2022-10-17 18:11:56 +0200

    Custom Filter

--- UD3/02_custom_filter/main.js
+++ UD3/02_custom_filter/main.js
@@ -1,6 +1,29 @@
 const assert = require('assert').strict;
 
-function select(array, condition) {
+function select(array, conditionCallback) {
+    let result = []
+
+    for (let index = 0; index < array.length; index++) {
+        const element = array[index];
+
+        if (conditionCallback(element)) {
+            result.push(element)
+        }
+    }
+
+    return result
+}
+
+function pairs(element){
+    return element % 2 ==0
+}
+
+function gt15(element){
+    return element>15
+}
+
+function lt10(element){
+    return element < 10
 }
 
 let values = [1, 2, 3, 5, 7, 13, 17, 23, 29]
