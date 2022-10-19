b9d872bba456df657db2c696b082515fc9943112
DESKTOP-S6DAD6J\A8-PC100 <asd@svalero.com>
2022-10-19 20:52:15 +0200

    UD3EJ5

---
+++ UD3/05_dom/main.js
@@ -0,0 +1,8 @@
+let allDivs = document.getElementsByTagName("div")
+console.log(allDivs)
+let styleButton = document.querySelectorAll("button")
+console.log(styleButton)
+let divButton = document.querySelector("div.buttons")
+console.log(divButton)
+let allDivsButton = document.querySelectorAll("div.buttons")
+console.log(allDivsButton)
