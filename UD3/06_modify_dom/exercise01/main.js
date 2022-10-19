45443f44b8d149efa46382c2596cad5842a6fd24
DESKTOP-S6DAD6J\A8-PC100 <asd@svalero.com>
2022-10-19 21:00:57 +0200

    UD3EJ6/1 sin acabar

--- UD3/06_modify_dom/exercise01/main.js
+++ UD3/06_modify_dom/exercise01/main.js
@@ -1,5 +1,13 @@
 window.onload = function(e) {
     console.log('documento cargado')
+    //Cambiar el título del segundo h2
+    e.innerText("h2")
+    //Seleccionar el elemento con id == username
+    //Cambiar el color de todos los .first que estén dentro de un artículo
+    //Seleccionar todos lo elementos li con class == item
+    //Seleccionar todos lo buttons dentro de class == buttons
+    //Cambiar el código de fondo del primer párrafo
+    //Cambiar el color de frente (forecolor) de los elementos buttons dentro de class == buttons
 
     // document.getElementsByTagName('h1')[0].innerText = 'Changed from JS!!'
 }
