06c1227a9809cc047bedd738ea64bf7e47e4977a
Daniel Alonso <a26129@svalero.com>
2022-10-26 22:59:16 +0200

    UD3EJ6-1

--- UD3/06_modify_dom/exercise01/main.js
+++ UD3/06_modify_dom/exercise01/main.js
@@ -1,13 +1,20 @@
 window.onload = function(e) {
     console.log('documento cargado')
-    //Cambiar el título del segundo h2
-    e.innerText("h2")
-    //Seleccionar el elemento con id == username
-    //Cambiar el color de todos los .first que estén dentro de un artículo
-    //Seleccionar todos lo elementos li con class == item
-    //Seleccionar todos lo buttons dentro de class == buttons
-    //Cambiar el código de fondo del primer párrafo
-    //Cambiar el color de frente (forecolor) de los elementos buttons dentro de class == buttons
-
-    // document.getElementsByTagName('h1')[0].innerText = 'Changed from JS!!'
+        //Cambiar el título del segundo h2
+    document.getElementsByTagName('h2')[1].innerHTML = "Texto modificado"
+        //Seleccionar el elemento con id == username
+    console.log(document.getElementById('username'))
+        //Cambiar el color de todos los .first que estén dentro de un artículo
+    console.log(document.querySelectorAll('article'))
+    document.querySelectorAll('article>.first').forEach(first => first.style.color = "green")
+        //Seleccionar todos lo elementos li con class == item
+    console.log(document.querySelectorAll('li.item'))
+        //Seleccionar todos lo buttons dentro de class == buttons
+    console.log(document.querySelectorAll('.buttons>button'))
+        //Cambiar el código de fondo del primer párrafo
+    var p1 = document.getElementsByTagName('p')[0]
+    p1.style.backgroundColor = "blue"
+        //Cambiar el color de frente (forecolor) de los elementos buttons dentro de class == buttons
+    document.querySelectorAll('.buttons>button').forEach(buttons => buttons.style.color = "blue")
+        // document.getElementsByTagName('h1')[0].innerText = 'Changed from JS!!'
 }
